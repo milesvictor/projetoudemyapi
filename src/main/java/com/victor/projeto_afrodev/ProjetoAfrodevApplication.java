2497e5e5794c9add0072b6f43b7f1f1ecbf9944a
Victor de Oliveira <victorbang013@gmail.com>
2020-12-03 22:58:15 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/victor/projeto_afrodev/ProjetoAfrodevApplication.java
@@ -0,0 +1,13 @@
+package com.victor.projeto_afrodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAfrodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAfrodevApplication.class, args);
+	}
+
+}
