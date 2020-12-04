2a9e62451c09d3f4889e0146f27e4e04f2394dcc
Victor de Oliveira <victorbang013@gmail.com>
2020-12-04 14:48:38 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/victor/projeto_afrodev/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.victor.projeto_afrodev.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.victor.projeto_afrodev.domain.User;
+import com.victor.projeto_afrodev.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		userReposiroty.deleteAll();
+		
+		User maria= new User(null, "Maria Brown", "maria@gmail.com");
+		User alex= new User(null, "Alex Green", "alex@gmail.com");
+		User bob= new User(null, "BobGrey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+	
+	
+}
