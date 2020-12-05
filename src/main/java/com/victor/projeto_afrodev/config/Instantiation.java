97130b9f7198221c357c701d5216e92010e8358d
Victor de Oliveira <victorbang013@gmail.com>
2020-12-04 21:37:39 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/victor/projeto_afrodev/config/Instantiation.java
+++ src/main/java/com/victor/projeto_afrodev/config/Instantiation.java
@@ -22,7 +22,7 @@ public class Instantiation implements CommandLineRunner {
 		
 		User maria= new User(null, "Maria Brown", "maria@gmail.com");
 		User alex= new User(null, "Alex Green", "alex@gmail.com");
-		User bob= new User(null, "BobGrey", "bob@gmail.com");
+		User bob= new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 	}
--- src/main/java/com/victor/projeto_afrodev/resources/UserResource.java
+++ src/main/java/com/victor/projeto_afrodev/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.victor.projeto_afrodev.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victor.projeto_afrodev.domain.User;
+import com.victor.projeto_afrodev.dto.UserDTO;
 import com.victor.projeto_afrodev.services.UserService;
 
 
@@ -22,11 +24,13 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping //Método GET para mostrar os valores
-	public ResponseEntity<List<User>> findAll() { //Retorna resposta http
+	public ResponseEntity<List<UserDTO>> findAll() { //Retorna resposta http
 		
 		List<User> list = service.findAll();
 		
-		return ResponseEntity.ok().body(list); //Código com respota de sucesso junto com o list
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto); //Código com respota de sucesso junto com o list
 
 	}
 			
