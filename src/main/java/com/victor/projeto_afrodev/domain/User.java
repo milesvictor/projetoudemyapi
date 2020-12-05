8d727a3216fe61d05a662ef01428a5c080c58abc
Victor de Oliveira <victorbang013@gmail.com>
2020-12-05 01:42:39 -0300

    Referenciando os posts do usuario

--- src/main/java/com/victor/projeto_afrodev/domain/User.java
+++ src/main/java/com/victor/projeto_afrodev/domain/User.java
@@ -5,8 +5,11 @@
 package com.victor.projeto_afrodev.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -16,10 +19,15 @@ public class User implements Serializable { //Serializable necessário para conv
 	
 	private static final long serialVersionUID = 1L; // Número de versão padrão do Serializable.
 	
+	
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	
 	public User() {
@@ -70,6 +78,19 @@ public class User implements Serializable { //Serializable necessário para conv
 		
 		this.email = email;
 	}
+	
+	
+	public List<Post> getPosts() {
+		
+		return posts;
+	}
+
+
+	public void setPosts(List<Post> posts) {
+		
+		this.posts = posts;
+	}
+
 
 	//Métodos hashCode e equals para que os objetos possam ser comparáveis.
 	@Override
