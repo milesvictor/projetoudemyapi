7f1f65ff2e820e85b77f0c8e10d4965aa6ff3055
Victor de Oliveira <victorbang013@gmail.com>
2020-12-05 02:26:46 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/victor/projeto_afrodev/domain/Post.java
+++ src/main/java/com/victor/projeto_afrodev/domain/Post.java
@@ -6,12 +6,15 @@ package com.victor.projeto_afrodev.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.victor.projeto_afrodev.dto.AuthorDTO;
+import com.victor.projeto_afrodev.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -25,6 +28,9 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -101,6 +107,18 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		
 		this.author = author;
+	}
+
+	
+	public List<CommentDTO> getComments() {
+		
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		
+		this.comments = comments;
 	}
 
 
