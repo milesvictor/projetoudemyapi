a1bd624777745f3cc8185c1240338ac593bdec4c
Victor de Oliveira <victorbang013@gmail.com>
2020-12-05 01:26:59 -0300

    Criando entity Post com User alinhado

---
+++ src/main/java/com/victor/projeto_afrodev/repository/PostRepository.java
@@ -0,0 +1,16 @@
+/*
+	Classe de repositório da postagem. Operaçõpes como salvar, recuperar deletar, atualizar etc.
+*/
+
+package com.victor.projeto_afrodev.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victor.projeto_afrodev.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+}
